5b48171e0327477468e6411c16f2d74f2cb46811
Carlos Lantigua <clantigua954@gmail.com>
2018-07-28 15:23:04 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 // create variables that represent cards
-const cardOne = "queen";
-const cardTwo = "king";
-const cardthree = "queen";
-const cardfour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+console.log("The user just flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("The user just flipped " + cardOne);
+
+
+
+
+if (cardsInPlay.length === 2) {
+  if (cardOne === cardTwo) {
+    alert(`You matched ${cardOne} with ${cardTwo}!!`);
+  } else {
+    alert("Sorry, try again");
+  }
+}
