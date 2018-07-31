a67332447e6af873c6c0e50b681a00d4f5b166ba
Carlos Lantigua <clantigua954@gmail.com>
2018-07-30 21:24:09 -0400

    add card object with rank, suit, and cardImage location

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 // create variables that represent cards
-const cards = ["queen", "queen", "king", "king"];
+const cards = [{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }
+];
+
+
 let cardsInPlay = []; //stores cards user flipped
 
 //hold selected cards and give feedback to user
 let flipCard = function (cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 };
 
 let checkForMatch = function () { //Check if 2 cards have been selected
@@ -21,4 +45,3 @@ let checkForMatch = function () { //Check if 2 cards have been selected
 flipCard(0);
 flipCard(3);
 checkForMatch();
-console.log(cardsInPlay);
